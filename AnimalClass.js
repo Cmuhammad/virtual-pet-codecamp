dad3ddf090dd73f90028ecd506bcf5206d505d72
Jalil1103 <abdijalildini@protonmail.com>
2019-09-06 14:13:09 -0400

    what we have so far?

--- AnimalClass.js
+++ AnimalClass.js
@@ -1,15 +1,19 @@
 class AnimalClass{
+
+    constructor(){
+        this.emotion = [];
+    }
    
     setOrganic(bool){
         return bool;
-       
-   }
-   hungerLevel(currentHunger){
-       // 1 is starving , 2 is hungry, 3 is peckish, 0 is dead
-       switch(currentHunger){
-           case 1:
-               return "starving";
-               break;
+        
+    }
+    hungerLevel(currentHunger){
+        // 1 is starving , 2 is hungry, 3 is peckish, 0 is dead
+        switch(currentHunger){
+            case 1:
+                return "starving";
+                break;
             case 2:
                 return "hungry"
                 break;
@@ -19,10 +23,28 @@ class AnimalClass{
             default:
                 return "dead";
                 break;
-       }
+        }
 
+    }
+    energyLevel(currentEnergy)
+    {
+        if(currentEnergy >= 76 && currentEnergy<= 100){
+            return "rested";
+        }else if( currentEnergy >=51 && currentEnergy<=75){
+            return "okay";
+        }else if ( currentEnergy >=26 && currentEnergy <=50 ){
+            return "sleepy";
+        }else if ( currentEnergy >1 && currentEnergy <=25){
+            return "exhuasted";
+        }else{
+            return "ded";
+        }
+    }
+    currentEmotion(emotion){
+        
 
-   }
+    }
+    
 
 
 
--- Animalclass.test.js
+++ Animalclass.test.js
@@ -12,9 +12,48 @@ describe("AnimalClass",() =>{
         })
     })
     describe("isHungry", () =>{
-        test("does hunger work?", () => {
+        test("level1", () => {
+            const actual = underTest.hungerLevel(1);
+            expect(actual).toBe("starving");
+        })
+        test("level2", () => {
             const actual = underTest.hungerLevel(2);
             expect(actual).toBe("hungry");
         })
+        test("level 3", () => {
+            const actual = underTest.hungerLevel(3);
+            expect(actual).toBe("peckish");
+        })
+        test(" level 3", () => {
+            const actual = underTest.hungerLevel(0);
+            expect(actual).toBe("dead");
+        })
+    })
+    describe("Energy levels work?", () =>{
+         test("does print it rested", () => {
+             let actual = underTest.energyLevel(76);
+             expect(actual).toBe("rested");
+
+          })
+         test("does print it okay", () => {
+             let actual = underTest.energyLevel(51);
+             expect(actual).toBe("okay");
+
+         })
+         test("does print it sleepy", () => {
+            let actual = underTest.energyLevel(26);
+            expect(actual).toBe("sleepy");
+
+         })
+         test("does print it exhausted", () => {
+            let actual = underTest.energyLevel(25);
+             expect(actual).toBe("exhuasted");
+
+         })
+         test("does print it ded", () => {
+            let actual = underTest.energyLevel(-3);
+             expect(actual).toBe("ded");
+
+         })
     })
 })
