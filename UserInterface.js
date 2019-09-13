1f27495b2d49fec441ac917b94d24520761145c0
Jalil1103 <abdijalildini@protonmail.com>
2019-09-13 15:07:04 -0400

    Merging

---
+++ UserInterface.js
@@ -0,0 +1,25 @@
+const Shelter = require('./Shelter');
+const Robot = require('./Robotic');
+const Organic = require('./Organic');
+const Animal = require('./Animal');
+
+
+console.log("Welcome to Your Virtual Pet! ");
+console.log("-----------------------------");
+
+let userSelection = input.question("Do You Want A Cat or Dog? ");
+let petName = input.question("what is your pet's name? ");
+let isOrganic = input.question("How about a Robot? ");
+
+
+// if(userSelection.toLowerCase() === "cat"){
+
+
+// }
+// if(userSelectio.toLowerCase() = "dog")
+console.log("-----------------------------");
+
+
+console.log("");
+console.log("");
+
